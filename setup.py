7aa71087f557dc897d001e73ceb748f90df54261
JustFineNeuro <57117067+JustFineNeuro@users.noreply.github.com>
2025-03-31 15:33:59 -0500

    push

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the README.md
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text()
 
 setup(
     name="BayesBrain",
     version="0.1.0",
     author="Your Name",
     description="Bayesian Generalized linear modeling and GAMs utilizing NumPyro",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # <-- tells PyPI it's Markdown
     packages=find_packages(),
     install_requires=[
         "jax[cpu]",
@@ -16,8 +23,7 @@ setup(
         "numpy",
         "patsy",
         "scikit-learn",
-        "scipy",
-        "patsy"
+        "scipy"
     ],
     python_requires=">=3.8",
 )
